5411c8c6ca1e8b6190966aa0990312f2a3ac3434
Jordan <jordan.imran@predixa.ai>
2022-04-30 13:25:47 +0400

    init

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+      <div className='w-screen h-screen flex items-center justify-center'>
+        Hello from NextJS
+      </div>
+    )
+}
+
+export default Home
